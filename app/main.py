e5548e9967ef8c3c9ae403a42302eb3f065d0a7d
Oleh <narijore@gmail.com>
2025-10-04 21:12:10 +0100

    add classes anf method serve_cars

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Car:
     # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
     # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = round(average_rating, 1)
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, brands: list) -> None:
+        res = 0
+        for car in brands:
+            if car.clean_mark < self.clean_power:
+                car.clean_mark = self.clean_power
+                res += car.clean_mark * self.average_rating
+        return res
